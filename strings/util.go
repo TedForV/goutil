d5ef2e0c8c1c400cf60755a4a46061a499112ab2
TedForV <TedForV@outlook.com>
2018-05-21 11:52:15 +0800

    add IsValidMobile func

--- strings/util.go
+++ strings/util.go
@@ -1,11 +1,20 @@
 package strings
 
+import "regexp"
+
 func Reverse(s string) string {
 	if len(s) == 0 {
 		return s
 	}
 	r := []rune(s)
 	return string(reverse(r))
+}
+
+const MOBILE_REGEX = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$"
+
+func IsValidMobile(mobileNo string) bool {
+	reg := regexp.MustCompile(MOBILE_REGEX)
+	return reg.MatchString(mobileNo)
 }
 
 func reverse(r []rune) []rune {
--- strings/util_test.go
+++ strings/util_test.go
@@ -10,3 +10,10 @@ func TestReverse(t *testing.T) {
 	result := Reverse(s)
 	assert.Equal(t, "家大和dlrow tihs", result, "they should be reversed")
 }
+
+func TestIsValidMobile(t *testing.T) {
+	s := "18503060972"
+	assert.Equal(t, true, IsValidMobile(s), "is valid")
+	s = "185030609721"
+	assert.Equal(t, false, IsValidMobile(s), "is not valid")
+}
