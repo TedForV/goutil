8de867256f384bb1be8ae72e0f4108b1ce7ad1ee
TedForV <TedForV@outlook.com>
2018-08-11 17:43:10 +0800

    add string key & sorted set key

--- redis/redis.go
+++ redis/redis.go
@@ -1,13 +1,121 @@
 package redis
 
 import (
-	redis2 "github.com/gomodule/redigo/redis"
+	"fmt"
+	"strconv"
 	"time"
+
+	redis2 "github.com/gomodule/redigo/redis"
+	"github.com/json-iterator/go"
+	"github.com/pkg/errors"
 )
 
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+const (
+	// OrderByASC const string for asc order
+	OrderByASC = "ASC"
+	// OrderByDESC const string for desc order
+	OrderByDESC = "DESC"
+)
+
+// CreateRedisConn create a connection to redis
 func CreateRedisConn(url string, connTimeout time.Duration, readTimeout time.Duration, writeTimeout time.Duration) (redis2.Conn, error) {
 	return redis2.Dial("tcp", url,
 		redis2.DialConnectTimeout(connTimeout),
 		redis2.DialReadTimeout(readTimeout),
 		redis2.DialWriteTimeout(writeTimeout))
+}
+
+// SetString set a value to the string key
+func SetString(conn *redis2.Conn, key string, data interface{}, expiredSeconds int) error {
+	var (
+		value string
+		err   error
+	)
+
+	switch data.(type) {
+	case string:
+		value = data.(string)
+	default:
+		value, err = json.MarshalToString(data)
+	}
+	if err != nil {
+		return errors.Wrap(err, "SetString failed.")
+	}
+	_, err = (*conn).Do("SETEX", key, expiredSeconds, value)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("SET failed. Key:%s Value:%s", key, value))
+	}
+	return nil
+}
+
+// GetString get a value from a string key
+func GetString(conn *redis2.Conn, key string, data interface{}) error {
+	value, err := redis2.String((*conn).Do("GET", key))
+	if err != nil {
+		return errors.Wrap(err, "GetString failed.")
+	}
+	return json.UnmarshalFromString(value, data)
+}
+
+// AddSortedSet add a data into a sorted set
+func AddSortedSet(conn *redis2.Conn, key string, data interface{}, score int64) error {
+	var value string
+	var err error
+	switch data.(type) {
+	case string:
+		value = data.(string)
+	case int:
+		value = strconv.Itoa(data.(int))
+	default:
+		value, err = json.MarshalToString(data)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("%+v", data))
+		}
+	}
+	_, err = (*conn).Do("ZADD", key, score, value)
+	if err != nil {
+		return errors.Wrap(err, fmt.Sprintf("ZADD failed. Key:%s Value:%s Score:%d", key, value, score))
+	}
+	return nil
+}
+
+// GetSortSet get a range data from sorted set key
+func GetSortSet(conn *redis2.Conn, key string, pageNo int, pageRow int, isDESC bool) ([]string, error) {
+	var order string
+	if isDESC {
+		order = OrderByDESC
+	} else {
+		order = OrderByASC
+	}
+	value, err := redis2.Strings((*conn).Do("SORT", key, "LIMIT", 0, 10, order))
+	_ = order
+	//value, err := redis2.Strings((*conn).Do("SORT", key, fmt.Sprintf(" LIMIT %d %d ", pageRow*(pageNo-1), pageRow), order))
+	//fmt.Printf(" LIMIT %d %d ", pageRow*(pageNo-1), pageRow)
+	//value, err := redis2.Strings((*conn).Do("SORT", key, order))
+	if err != nil {
+		return value, errors.Wrap(err, fmt.Sprintf("GetSortSet failed. Key: %s pageNo: %d pageRow: %d isDESC: %t", key, pageNo, pageRow, isDESC))
+	}
+	return value, nil
+
+}
+
+// DeleteSortSetItem delete the item from a sorted set
+func DeleteSortSetItem(conn *redis2.Conn, key string, data interface{}) error {
+	var value string
+	var err error
+	switch data.(type) {
+	case string:
+		value = data.(string)
+	case int:
+		value = strconv.Itoa(data.(int))
+	default:
+		value, err = json.MarshalToString(data)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("%+v", data))
+		}
+	}
+	_, err = (*conn).Do("ZREM", key, value)
+	return err
 }
--- redis/redis_test.go
+++ redis/redis_test.go
@@ -3,7 +3,12 @@ package redis
 import (
 	"testing"
 	"time"
+
+	"github.com/gomodule/redigo/redis"
+	"github.com/stretchr/testify/assert"
 )
+
+var testPrefixKey = "test:"
 
 func TestCreateRedisConn(t *testing.T) {
 	conn, err := CreateRedisConn("192.168.10.128:6379", time.Second*3, time.Second*10, time.Second*20)
@@ -11,4 +16,78 @@ func TestCreateRedisConn(t *testing.T) {
 		t.Error(err)
 	}
 	t.Log(conn)
+}
+
+func TestSetString(t *testing.T) {
+	conn, err := newConn()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	key := testPrefixKey + "1"
+	input := model{
+		ID:     1,
+		Name:   "大声的发送的",
+		Age:    20,
+		Remark: "大声的发现擦拭豆腐干发二娃如果",
+	}
+	SetString(&conn, key, &input, 10)
+	var result model
+	GetString(&conn, key, &result)
+	assert.Equal(t, input.Name, result.Name)
+	assert.Equal(t, input.Age, result.Age)
+}
+
+func TestAddSortedSet(t *testing.T) {
+	conn, err := newConn()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	key := "sortedsettest:1"
+	for i := 1; i < 100; i++ {
+		AddSortedSet(&conn, key, i, int64(i))
+	}
+}
+
+func TestGetSortSet(t *testing.T) {
+	conn, err := newConn()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	key := "sortedsettest:1"
+	data, err := GetSortSet(&conn, key, 1, 10, true)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for i, v := range data {
+		t.Logf("%d %s", i, v)
+	}
+}
+
+func TestDeleteSortSetItem(t *testing.T) {
+	conn, err := newConn()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	key := "sortedsettest:1"
+	err = DeleteSortSetItem(&conn, key, 95)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func newConn() (redis.Conn, error) {
+	return CreateRedisConn("192.168.10.128:6379", time.Second*3, time.Second*10, time.Second*20)
+}
+
+type model struct {
+	ID     int32  `json:"id"`
+	Name   string `json:"name"`
+	Age    int32  `json:"age"`
+	Remark string `json:"remark"`
 }
