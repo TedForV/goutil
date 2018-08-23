228a07ea292076f70ce1eb9a28724ec5778c5a81
TedForV <TedForV@outlook.com>
2018-08-23 18:48:52 +0800

    remove unit test

--- log/logrus.hooks/filehook/hook_test.go
+++ log/logrus.hooks/filehook/hook_test.go
@@ -1,19 +1,13 @@
 package filehook
 
-import (
-	"github.com/TedForV/goutil/log/logrus.hooks"
-	"github.com/sirupsen/logrus"
-	"testing"
-)
+// func TestFileHook_Fire(t *testing.T) {
+// 	hook := NewFileHook("D:\\Test\\")
+// 	logrus.AddHook(hook)
 
-func TestFileHook_Fire(t *testing.T) {
-	hook := NewFileHook("D:\\Test\\")
-	logrus.AddHook(hook)
-
-	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Debug("debug test")
-	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Info("info test")
-	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Warn("warn test")
-	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Error("error test")
-	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Fatal("fatal test")
-	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Panic("panic test")
-}
+// 	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Debug("debug test")
+// 	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Info("info test")
+// 	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Warn("warn test")
+// 	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Error("error test")
+// 	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Fatal("fatal test")
+// 	logrus.WithField(logrushooks.ERROR_ADDINFO_NAME, "test").WithField(logrushooks.ERROR_TRACE_NAME, "test").Panic("panic test")
+// }
