23ebf61902db6fbc0a113881f3e978c2d041e504
TedForV <TedForV@outlook.com>
2019-12-01 11:55:00 +0800

    add recovery. Stack method for recovery

---
+++ base/recovery.go
@@ -0,0 +1,76 @@
+package base
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"runtime"
+	"time"
+)
+
+var (
+	dunno     = []byte("???")
+	slash     = []byte("/")
+	centerDot = []byte("·")
+	dot       = []byte(".")
+)
+
+// Stack for exception stack details
+func Stack(skip int) []byte {
+	buf := new(bytes.Buffer)
+	var lines [][]byte
+	var lastFile string
+	for i := skip; ; i++ {
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+		if file != lastFile {
+			data, err := ioutil.ReadFile(file)
+			if err != nil {
+				continue
+			}
+			lines = bytes.Split(data, []byte{'\n'})
+			lastFile = file
+		}
+		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
+	}
+	return buf.Bytes()
+}
+
+func source(lines [][]byte, n int) []byte {
+	n--
+	if n < 0 || n > len(lines) {
+		return dunno
+	}
+	return bytes.TrimSpace(lines[n])
+}
+
+func function(pc uintptr) []byte {
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return dunno
+	}
+	name := []byte(fn.Name())
+	// The name includes the path name to the package, which is unnecessary
+	// since the file name is already included.  Plus, it has center dots.
+	// That is, we see
+	//	runtime/debug.*T·ptrmethod
+	// and want
+	//	*T.ptrmethod
+	// Also the package path might contains dot (e.g. code.google.com/...),
+	// so first eliminate the path prefix
+	if lastSlash := bytes.LastIndex(name, slash); lastSlash >= 0 {
+		name = name[lastSlash+1:]
+	}
+	if period := bytes.Index(name, dot); period >= 0 {
+		name = name[period+1:]
+	}
+	name = bytes.Replace(name, centerDot, dot, -1)
+	return name
+}
+
+func timeFormat(t time.Time) string {
+	return t.Format("2006/01/02 15:04:05")
+}
